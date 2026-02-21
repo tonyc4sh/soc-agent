d08b69cc9216b96d10709874356880885bcce155
tonyc4sh <franciszekglab695@gmail.com>
2026-02-21 02:30:08 +0100

    parser fix

--- parser.py
+++ parser.py
@@ -2,13 +2,14 @@ class SSHParser:
 
     @staticmethod
     def parse(line: str):
+
         if "Failed password" in line:
             return {
                 "event": "FAILED_LOGIN",
                 "raw": line
             }
 
-        if "Accepted password" in line:
+        if "Accepted" in line and "sshd" in line:
             return {
                 "event": "SUCCESS_LOGIN",
                 "raw": line
