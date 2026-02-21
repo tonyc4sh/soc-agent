c2e0e12e80035bcd12b53e8424013895f08a90c5
tonyc4sh <franciszekglab695@gmail.com>
2026-02-21 02:34:28 +0100

    parser fix v2

--- parser.py
+++ parser.py
@@ -1,11 +1,17 @@
+import re
+
 class SSHParser:
 
     @staticmethod
     def parse(line: str):
 
         if "Failed password" in line:
+            ip_match = re.search(r'from (\d+\.\d+\.\d+\.\d+)', line)
+            ip = ip_match.group(1) if ip_match else None
+
             return {
                 "event": "FAILED_LOGIN",
+                "ip": ip,
                 "raw": line
             }
 
