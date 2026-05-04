529df25827f4eeb35e8230377ec36282db9a7c3c
tonyc4sh <franciszekglab695@gmail.com>
2026-05-05 01:25:28 +0200

    feat: replace polling log watcher with eventdriv watchdog(inotify)

--- watcher/log_watcher.py
+++ watcher/log_watcher.py
@@ -1,4 +1,33 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 import time
+import os
+
+
+class _LogHandler(FileSystemEventHandler):
+
+    def __init__(self, logfile):
+        self.logfile = os.path.abspath(logfile)
+        self._file = open(self.logfile)
+        self._file.seek(0, 2)
+        self._buffer = []
+
+    def on_modified(self, event):
+        
+        if os.path.basename(event.src_path) != "auth.log":
+            return
+
+        while True:
+            line = self._file.readline()
+            if not line:
+                break
+            self._buffer.append(line)
+
+    def get_lines(self):
+        lines = self._buffer[:]
+        self._buffer.clear()
+        return lines
+
 
 class LogWatcher:
 
@@ -6,11 +35,24 @@ class LogWatcher:
         self.logfile = logfile
 
     def follow(self):
-        with open(self.logfile) as f:
-            f.seek(0, 2)
+
+        handler = _LogHandler(self.logfile)
+
+        observer = Observer()
+        log_dir = os.path.dirname(self.logfile)
+
+        observer.schedule(handler, path=log_dir, recursive=False)
+        observer.start()
+
+        try:
             while True:
-                line = f.readline()
-                if not line:
-                    time.sleep(0.5)
-                    continue
-                yield line
+                lines = handler.get_lines()
+
+                for line in lines:
+                    yield line
+
+                time.sleep(0.1)
+
+        finally:
+            observer.stop()
+            observer.join()
